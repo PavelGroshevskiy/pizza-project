a8ae0bdc47bdca0220391e399fd5c5ac1821feb4
Pavel Groshevskiy <pavel.groshevskiy@gmail.com>
2022-06-23 11:42:22 +0700

    add routing, create sort component and cart page

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
-const Categories = () => {
+const Categories = ({ items, onClickItem }) => {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          className={activeItem === null ? "active" : ""}
+          onClick={() => {
+            onSelectItem(null);
+          }}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((el, idx) => (
+            <li
+              className={activeItem === idx ? "active" : ""}
+              onClick={() => onSelectItem(idx)}
+              key={`${el}_${idx}`}
+            >
+              {el}
+            </li>
+          ))}
       </ul>
     </div>
   );
